49cbcfe579793cbc104242da1f4d2ef2e7c3c92c
con-eng <cenglish2305@gmail.com>
2026-06-14 02:58:45 -0400

    Initial Commit

---
+++ SpotThePhish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotThePhish
+//
+//  Created by Connor English on 6/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotThePhish/SpotThePhishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotThePhishApp.swift
+//  SpotThePhish
+//
+//  Created by Connor English on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotThePhishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
